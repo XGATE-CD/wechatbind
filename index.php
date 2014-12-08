1e2756382af7040e8abb2db9ba5e14a37daac31e
binleen <158497182@qq.com>
2014-12-08 15:59:47 +0800

    Commit the first file into git develop branch

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 14-12-8
+ * Time: 下午3:51
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo 'This is the testing from Liu Qin<br/>';
+
+phpinfo();
+
+?>
